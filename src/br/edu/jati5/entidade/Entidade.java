4a9b4e71ae563bc7ca5a93d44a815e0926a99927
Aluno <Aluno@Aluno-PC>
2019-09-18 15:08:48 -0300

    Ataulizacao na Entidade

--- src/br/edu/jati5/entidade/Entidade.java
+++ src/br/edu/jati5/entidade/Entidade.java
@@ -1,6 +1,23 @@
 package br.edu.jati5.entidade;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Entidade {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 }
