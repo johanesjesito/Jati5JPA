2b84c09abebd4b18ef9acc02a2b2610a00efe666
Aluno <Aluno@Aluno-PC>
2019-09-18 15:25:57 -0300

    Modificacao na Pesquisa

--- src/br/edu/jati5/entidade/Pesquisa.java
+++ src/br/edu/jati5/entidade/Pesquisa.java
@@ -3,15 +3,79 @@ package br.edu.jati5.entidade;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "pesquisa")
 public class Pesquisa {
 
+	@Column
 	private String titulo;
+	@Column
 	private String descricao;
 
+	@ManyToOne
+	@JoinColumn(name = "usuario_fk", nullable = false)
 	private Usuario usuario;
+	@Column(name = "data_inicio")
 	private LocalDate dataInicio;
+	@Column(name = "data_fim")
 	private LocalDate dataFim;
 	
+	@Transient
 	private List<EstruturaPesquisa> estruturas;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public List<EstruturaPesquisa> getEstruturas() {
+		return estruturas;
+	}
+
+	public void setEstruturas(List<EstruturaPesquisa> estruturas) {
+		this.estruturas = estruturas;
+	}
 	
 }
