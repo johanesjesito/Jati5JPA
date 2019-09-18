49d038616c7fd5c5b577872014892384d0f9430e
Aluno <Aluno@Aluno-PC>
2019-09-18 15:46:43 -0300

    Atualizacao

--- src/br/edu/jati5/entidade/Dado.java
+++ src/br/edu/jati5/entidade/Dado.java
@@ -1,5 +1,20 @@
 package br.edu.jati5.entidade;
 
-public class Dado {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "dado")
+public class Dado extends Entidade{
+
+	@Column
+	private String col_1_nome_familia, col_2_nome, col_3_valor;
+	
+	@ManyToOne
+	@JoinColumn(name = "estrutura_pesquisa_jk")
+	private EstruturaPesquisa estruturaPesquisa;
+	
 }
--- src/br/edu/jati5/entidade/EstruturaPesquisa.java
+++ src/br/edu/jati5/entidade/EstruturaPesquisa.java
@@ -1,6 +1,28 @@
 
 package br.edu.jati5.entidade;
 
-public class EstruturaPesquisa {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "estrutura_pesquisa")
+public class EstruturaPesquisa extends Entidade{
+
+	@Column(nullable = true)
+	private String col_1_nome_familia;
+	@Column(nullable = false)
+	private String col_2_nome, col_3_valor, categoria_dados, titulo_estrutura;
+	@ManyToOne
+	@JoinColumn(name = "pesquisa_tk", nullable = false)
+	private Pesquisa pesquisa;
+	
+	@Transient
+	private List<Dado> dados;
+	
 }
