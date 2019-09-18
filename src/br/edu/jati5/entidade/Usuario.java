ff74f61e31ec7d8e51e7b6dce09ee61a427cefa3
Aluno <Aluno@Aluno-PC>
2019-09-18 15:16:55 -0300

    Atualizacao no Usuario

--- src/br/edu/jati5/entidade/Usuario.java
+++ src/br/edu/jati5/entidade/Usuario.java
@@ -2,12 +2,49 @@ package br.edu.jati5.entidade;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 
+	@Column(nullable = false)
 	private String nome;
+	@Column(nullable = false, unique = true)
 	private String login;
+	@Column(nullable = false)
 	private String senha;
-	
+	@Transient
 	private List<Pesquisa> pesquisas;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public List<Pesquisa> getPesquisas() {
+		return pesquisas;
+	}
+	public void setPesquisas(List<Pesquisa> pesquisas) {
+		this.pesquisas = pesquisas;
+	}
 
+	
 }
